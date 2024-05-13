dbba62c10ad11b447b2c5213b6baa67aa161733b
khiemnguyen812 <ntkhiem21@clc.fitus.edu.vn>
2024-05-13 22:30:30 +0700

    Create Category window and CRUD Category

--- KandM Clothes/Areas/Admin/Controllers/CategoryController.cs
+++ KandM Clothes/Areas/Admin/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using KandM_Clothes.Models;
+using KandM_Clothes.Models.EF;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,9 +13,68 @@ namespace KandM_Clothes.Areas.Admin.Controllers
         // GET: Admin/Category
 
         ApplicationDbContext _dbContext = new ApplicationDbContext();
+
         public ActionResult Index()
+        {
+            var categories = _dbContext.Categories.ToList();
+            return View(categories);
+        }
+
+        public ActionResult Add()
         {
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Add(Category category)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            category.CreatedDate = DateTime.Now;
+            category.ModifiedDate = DateTime.Now;
+            _dbContext.Categories.Add(category);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var category = _dbContext.Categories.Find(id);
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Category category)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            _dbContext.Categories.Attach(category);
+            category.ModifiedDate = DateTime.Now;
+            _dbContext.Entry(category).Property(x => x.Title).IsModified = true;
+            _dbContext.Entry(category).Property(x =>
+            x.Description).IsModified = true;
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            var category = _dbContext.Categories.Find(id);
+            if (category == null)
+            {
+                return Json(new { success = false });
+            }
+            _dbContext.Categories.Remove(category);
+            _dbContext.SaveChanges();
+            return Json(new { success = true });
+        }
     }
+
 }
